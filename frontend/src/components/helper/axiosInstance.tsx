141af49e132b3f22ae59815cae951dd361b08b9b
paulSuman005 <sumanpauls568gmail.com>
2025-09-01 00:35:39 +0530

    axios updated

--- frontend/src/components/helper/axiosInstance.tsx
+++ frontend/src/components/helper/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5001';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const axiosInstance = axios.create();
 
